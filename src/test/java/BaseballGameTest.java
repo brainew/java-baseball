b17c65375739762713c458a8bba60caa5a6258b3
OJEW <brainew1@gmail.com>
2021-04-26 22:04:41 +0900

    test: 스타일 체크로 인한 수정

--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -1,11 +1,10 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class BaseballGameTest {
 
@@ -20,25 +19,24 @@ public class BaseballGameTest {
 
 		ArrayList<Integer> defaultNumbers = new ArrayList<>(linkedHashMap);
 
-		assertEquals(3, defaultNumbers.size());
+		Assertions.assertEquals(3, defaultNumbers.size());
 	}
 
 	@Test
 	@DisplayName("사용자로부터 입력받은 숫자 검증(size)")
 	void valid_size_numbers_isOK() {
-		ArrayList<Integer> userInputNumbers = new ArrayList<>(Arrays.asList(1,2,3));
-		boolean flag = true;
+		ArrayList<Integer> userInputNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-		assertEquals(3, userInputNumbers.size());
+		Assertions.assertEquals(3, userInputNumbers.size());
 	}
 
 	@Test
 	@DisplayName("사용자로부터 입력받은 숫자 검증(range)")
 	void valid_range_numbers_isOK() {
-		ArrayList<Integer> userInputNumbers = new ArrayList<>(Arrays.asList(1,2,3));
+		ArrayList<Integer> userInputNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-		for (int inputNumber: userInputNumbers) {
-			assertTrue((inputNumber > 0 && inputNumber < 10));
+		for (int inputNumber : userInputNumbers) {
+			Assertions.assertTrue((inputNumber > 0 && inputNumber < 10));
 		}
 	}
 
@@ -48,21 +46,21 @@ public class BaseballGameTest {
 		String inputNumbers = "123";
 		ArrayList<Integer> parseNumbers = new ArrayList<>();
 
-		for (char inputNumber: inputNumbers.toCharArray()) {
+		for (char inputNumber : inputNumbers.toCharArray()) {
 			parseNumbers.add(Integer.parseInt(String.valueOf(inputNumber)));
 		}
 
-		assertEquals(3, parseNumbers.size());
+		Assertions.assertEquals(3, parseNumbers.size());
 	}
 
 	@Test
 	@DisplayName("자리수 별 비교")
 	void check_ball_strike() {
-		ArrayList<Integer> defaultNumbers = new ArrayList<>(Arrays.asList(1,2,3));
-		ArrayList<Integer> inputNumbers = new ArrayList<>(Arrays.asList(1,2,3));
+		ArrayList<Integer> defaultNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+		ArrayList<Integer> inputNumbers = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-		assertEquals(0, isBall(defaultNumbers, inputNumbers));
-		assertEquals(3, isStrike(defaultNumbers, inputNumbers));
+		Assertions.assertEquals(0, isBall(defaultNumbers, inputNumbers));
+		Assertions.assertEquals(3, isStrike(defaultNumbers, inputNumbers));
 	}
 
 	int isBall(ArrayList<Integer> defaultNumbers, ArrayList<Integer> inputNumbers) {
@@ -80,7 +78,7 @@ public class BaseballGameTest {
 	int isStrike(ArrayList<Integer> defaultNumbers, ArrayList<Integer> inputNumbers) {
 		int strikeCount = 0;
 
-		for (int defaultNumber: defaultNumbers) {
+		for (int defaultNumber : defaultNumbers) {
 			strikeCount += (inputNumbers.get(defaultNumbers.indexOf(defaultNumber)).equals(defaultNumber)) ? 1 : 0;
 		}
 
