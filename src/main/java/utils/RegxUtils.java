b1fc66c0afda2bfa407373d488712afd99a382fe
OJEW <brainew1@gmail.com>
2021-04-27 21:40:00 +0900

    style: indent 관련 수정

--- src/main/java/utils/RegxUtils.java
+++ src/main/java/utils/RegxUtils.java
@@ -3,19 +3,19 @@ package utils;
 import java.util.regex.Pattern;
 
 public class RegxUtils {
-    public static boolean continueGameAnswerRegx(String validString) {
-        return Pattern.matches("^[1-2]*$", validString);
-    }
+	public static boolean continueGameAnswerRegx(String validString) {
+		return Pattern.matches("^[1-2]*$", validString);
+	}
 
-    public static boolean continueGameAnswerRangeRegx(String validString) {
-        return Pattern.matches("^\\d{1}$", validString);
-    }
+	public static boolean continueGameAnswerRangeRegx(String validString) {
+		return Pattern.matches("^\\d{1}$", validString);
+	}
 
-    public static boolean defaultGameAnswerRegx(String validString) {
-        return Pattern.matches("^[1-9]*$", String.valueOf(validString));
-    }
+	public static boolean defaultGameAnswerRegx(String validString) {
+		return Pattern.matches("^[1-9]*$", String.valueOf(validString));
+	}
 
-    public static boolean defaultGameAnswerRangeRegx(String validString) {
-        return Pattern.matches("^\\d{3}$", validString);
-    }
+	public static boolean defaultGameAnswerRangeRegx(String validString) {
+		return Pattern.matches("^\\d{3}$", validString);
+	}
 }
--- src/main/java/utils/ValidUtils.java
+++ src/main/java/utils/ValidUtils.java
@@ -2,8 +2,8 @@ package utils;
 
 public class ValidUtils {
 	public static boolean validInputNumbers(String inputNumber, int checkSize) {
-		return (checkSize == 3) ?
-				RegxUtils.defaultGameAnswerRegx(inputNumber) && RegxUtils.defaultGameAnswerRangeRegx(inputNumber)
-				: RegxUtils.continueGameAnswerRegx(inputNumber) && RegxUtils.continueGameAnswerRangeRegx(inputNumber);
+		return (checkSize == 3)
+			? RegxUtils.defaultGameAnswerRegx(inputNumber) && RegxUtils.defaultGameAnswerRangeRegx(inputNumber)
+			: RegxUtils.continueGameAnswerRegx(inputNumber) && RegxUtils.continueGameAnswerRangeRegx(inputNumber);
 	}
 }
